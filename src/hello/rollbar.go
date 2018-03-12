382241220d0162118822629087d4cefd1262cd56
Wahid Ali <wahid.ali@psquickit.com>
2018-03-13 00:14:13 +0530

    Committed by wahid

--- src/hello/rollbar.go
+++ src/hello/rollbar.go
@@ -1,10 +1,10 @@
 package main
 import (
   "github.com/rollbar/rollbar-go" 
-  "errors"
   "fmt"
+  "time" 
 ) 
-func catchError() {  
+func recoverError() {  
     if r := recover(); r!= nil {
 		rollbar.Critical(r)
         fmt.Println(r)
@@ -13,27 +13,11 @@ func catchError() {
 }
 
 func main() {
-	defer catchError()
+	defer recoverError()
 	rollbar.SetToken("69609226b4014ef39f62d4b2008c56ed")
 	rollbar.SetEnvironment("production") 
-	/*"time" 
 	var timer *time.Timer = nil
-	timer.Reset(10) // crash*/
-	
-	r := generateError("Test Exception")
-    if r != nil {
-        fmt.Println("recovered from ", r)
-		rollbar.Critical(r)
-        fmt.Println(r)
-		rollbar.Wait()
-        // Add Rollbar later here to report the error.
-    }
-	
-}
-
-func generateError(err string) error {
-
-    return errors.New(err)
+	timer.Reset(10) // crash
 }
 
 

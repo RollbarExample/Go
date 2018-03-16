627f924d1271fb4f4ea4c08cbdb513e45a798a8f
Wahid Ali <wahid.ali@psquickit.com>
2018-03-16 14:35:17 +0530

    Comitted by wahid

--- src/hello/squareRoot.go
+++ src/hello/squareRoot.go
@@ -4,22 +4,23 @@ import "fmt"
 import "errors"
 import "math"
 func main() {
-  ret, err  := calculateSqrt(-1)    
 
+	ret, err  := calculateSqrt(-1)    
     if err != nil {
-        fmt.Println(err)
-    }else{
-           fmt.Println("The square root is ", ret)
+		fmt.Println(err)
+    } else {
+		fmt.Println("The square root is ", ret)
     }
 
 }
 
 
 func calculateSqrt(f float64) (float64, error) {
- //return an error as second parameter if invalid input
+
+	//return an error as second parameter if invalid input
    if (f < 0) {
-     return float64(math.NaN()),  errors.New("Not able to take the square root of a negative number")
+		return float64(math.NaN()),errors.New("Not able to take the square root of a negative number")
    }
-//otherwise use default square root function
+	//otherwise use default square root function
   return math.Sqrt(f),nil
 }

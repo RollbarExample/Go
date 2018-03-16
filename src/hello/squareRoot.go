d9394cb5f33baa10ae8386641911bc0d7a5b7711
Wahid Ali <wahid.ali@psquickit.com>
2018-03-16 14:40:34 +0530

    Comitted by wahid

--- src/hello/squareRoot.go
+++ src/hello/squareRoot.go
@@ -15,7 +15,7 @@ func main() {
 func calculateSqrt(f float64) (float64, error) {
 	//return an error as second parameter if invalid input
 	if (f < 0) {
-		return float64(math.NaN()),errors.New("Not able to take the square root of a negative number")
+	return float64(math.NaN()),errors.New("Not able to take the square root of a negative number")
 	}
 	//otherwise use default square root function
 	return math.Sqrt(f),nil
